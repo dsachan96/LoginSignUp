308485d450376e1c5d67ac6054e9648127bd9ff5
Deepak <dsachan86@gmail.com>
2020-07-20 21:04:51 +0530

    Routing Completed

--- LoginSignUp/src/app/app-routing.module.ts
+++ LoginSignUp/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   { path: 'Login' , component : LoginComponent },
-  { path: 'SignUp' , component : SignUpComponent}
+  { path: 'Login/SignUp' , component : SignUpComponent},
+  { path: 'SignUp' , component : SignUpComponent },
+  { path: './' , component : AppComponent},
+  
 ];
 
 @NgModule({
